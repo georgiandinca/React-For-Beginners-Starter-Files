8a2db5c647042b1e49d8ac4f08328f62a8f5edab
Georgian Dinca <geo@magnify.ro>
2017-08-18 07:07:19 +0300

    finish main functionalities

---
+++ catch-of-the-day/src/base.js
@@ -0,0 +1,9 @@
+import Rebase from 're-base';
+
+const base = Rebase.createClass({
+	apiKey: "AIzaSyD68itoeAZHNA0_FXa4mDKavuDhHn7zgSA",
+    authDomain: "catch-of-the-day-geodinca.firebaseapp.com",
+    databaseURL: "https://catch-of-the-day-geodinca.firebaseio.com",
+});
+
+export default base;
